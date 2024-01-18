6c0d28ed07c709ffdf7c3190b6a862ccf2ac6973
kanth5 <mrajk5@gmail.com>
2024-01-18 20:05:58 +0530

    Various Pointcut expressions

--- movie-recommender-aop/src/main/java/com/learning/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
+++ movie-recommender-aop/src/main/java/com/learning/springaop/movierecommenderaop/aspect/AccessCheckAspect.java
@@ -14,6 +14,12 @@ public class AccessCheckAspect {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Before("execution(* com.learning.springaop.movierecommenderaop.business.*.*(..))")
+    //@Before("execution(String com.learning.springaop.movierecommenderaop..*.*(String))") -> calls using return type String and having a parameter String
+    //@Before("execution(* com.learning.springaop.movierecommenderaop..*.*Filtering(..))") -> calls having the name 'Filtering' in them
+    //@Before("execution(String com.learning.springaop.movierecommenderaop..*.*(..))") -> calls using return type of String
+    //@Before("execution(* com.learning.springaop.movierecommenderaop.business.*.*(String,..))") -> calls with String as the first argument along with others
+    //@Before("execution(* com.learning.springaop.movierecommenderaop..*.*Filtering(..)) ||
+    //          execution(String com.learning.springaop.movierecommenderaop..*.*(..))") -> combining pointcut expressions
     public void before(JoinPoint joinPoint) {
         //	logger.info("Intercepted method call");
         logger.info("Intercepted call before execution of: {}", joinPoint);
